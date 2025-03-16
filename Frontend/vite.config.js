14a90e061f9f01fa5828687c62e0e09214aba230
prasa08nna <prasannabsachar@gmail.com>
2025-03-16 19:02:18 +0530

    changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-import tailwindcss from '@tailwindcss/vite'
+export default (mode) => {
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000", 
-        changeOrigin: true,
-        secure: false,
+  const env = loadEnv(mode.mode, process.cwd());
+
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL, 
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-});
+  });
+};
